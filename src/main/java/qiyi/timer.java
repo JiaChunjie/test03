1df16aed55f7c372d5c3b948c100b908d34bdfd1
jiachunjie <jiachunjie@qiyi.com>
2021-03-04 12:17:44 +0800

    更改个数

--- src/main/java/qiyi/timer.java
+++ src/main/java/qiyi/timer.java
@@ -18,14 +18,5 @@ public class timer extends JFrame {
          timerService frame = new timerService("计时器");
         frame.pack();
         frame.setVisible(true);
-
-
-        timerService frame2 = new timerService("计时器2");
-        frame2.pack();
-        frame2.setVisible(true);
-
-        timerService frame3 = new timerService("计时器3");
-        frame3.pack();
-        frame3.setVisible(true);
     }
 }
